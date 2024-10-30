1218ae4da11cc57458db93201d13e2e966826338
mir ziauddin <141651215+mirziauddin@users.noreply.github.com>
2024-10-30 15:51:02 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
 import Banner from "./components/Banner/Banner";
@@ -5,18 +6,24 @@ import Subscribe from "./components/Subscribe/Subscribe";
 import Banner2 from "./components/Banner/Banner2";
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <main className="overflow-x-hidden bg-white text-dark">
-      <Hero />
-      <Services />
-      <Banner />
-      <Subscribe />
-      <Banner2 />
-      <Contact />
-      <Footer />
-    </main>
+    <Router>
+      <main className="overflow-x-hidden bg-white text-dark">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/banner" element={<Banner />} />
+          <Route path="/subscribe" element={<Subscribe />} />
+          <Route path="/banner2" element={<Banner2 />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </main>
+    </Router>
   );
 };
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,11 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import Blob from "../../assets/blob.svg";
 import HeroPng from "../../assets/hero.png";
 import { animate, motion } from "framer-motion";
+import Services from "../Services/Services";
+import Banner from "../Banner/Banner";
+import Subscribe from "../Subscribe/Subscribe";
+import Banner2 from "../Banner/Banner2";
+import Contact from "../Contact/Contact";
 
 export const FadeUp = (delay) => {
   return {
@@ -28,55 +33,63 @@ export const FadeUp = (delay) => {
 
 const Hero = () => {
   return (
-    <section className="bg-light overflow-hidden relative">
-      <Navbar />
-      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
-        {/* Brand Info */}
-        <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
-          <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
-            <motion.h1
-              variants={FadeUp(0.6)}
-              initial="initial"
-              animate="animate"
-              className="text-3xl lg:text-5xl font-bold !leading-snug"
-            >
-              Let's Learn to build a{" "}
-              <span className="text-secondary">Website</span> for your business
-            </motion.h1>
-            <motion.div
-              variants={FadeUp(0.8)}
-              initial="initial"
-              animate="animate"
-              className="flex justify-center md:justify-start"
-            >
-              <button className="primary-btn flex items-center gap-2 group">
-                Get Started
-                <IoIosArrowRoundForward className="text-xl group-hover:translate-x-2 group-hover:-rotate-45 duration-300" />
-              </button>
-            </motion.div>
+    <>
+      <section className="bg-light overflow-hidden relative">
+        <Navbar />
+        <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
+          {/* Brand Info */}
+          <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
+            <div className="text-center md:text-left space-y-10 lg:max-w-[400px]">
+              <motion.h1
+                variants={FadeUp(0.6)}
+                initial="initial"
+                animate="animate"
+                className="text-3xl lg:text-5xl font-bold !leading-snug"
+              >
+                Let's Learn to build a{" "}
+                <span className="text-secondary">Website</span> for your
+                business
+              </motion.h1>
+              <motion.div
+                variants={FadeUp(0.8)}
+                initial="initial"
+                animate="animate"
+                className="flex justify-center md:justify-start"
+              >
+                <button className="primary-btn flex items-center gap-2 group">
+                  Get Started
+                  <IoIosArrowRoundForward className="text-xl group-hover:translate-x-2 group-hover:-rotate-45 duration-300" />
+                </button>
+              </motion.div>
+            </div>
+          </div>
+          {/* Hero Image */}
+          <div className="flex justify-center items-center">
+            <motion.img
+              initial={{ x: 50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.4, ease: "easeInOut" }}
+              src={HeroPng}
+              alt=""
+              className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"
+            />
+            <motion.img
+              initial={{ x: -50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
+              src={Blob}
+              alt=""
+              className="absolute -bottom-32 w-[800px] md:w-[1500px] z-[1] hidden md:block"
+            />
           </div>
         </div>
-        {/* Hero Image */}
-        <div className="flex justify-center items-center">
-          <motion.img
-            initial={{ x: 50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.6, delay: 0.4, ease: "easeInOut" }}
-            src={HeroPng}
-            alt=""
-            className="w-[400px] xl:w-[600px] relative z-10 drop-shadow"
-          />
-          <motion.img
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
-            src={Blob}
-            alt=""
-            className="absolute -bottom-32 w-[800px] md:w-[1500px] z-[1] hidden md:block"
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+      <Services />
+      <Banner />
+      <Subscribe />
+      <Banner2 />
+      <Contact />
+    </>
   );
 };
 
